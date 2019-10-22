7843b35435e66283d2d5920fe9ad61880345962c
octagonalsquare <octagonalsquare.mc@gmail.com>
2019-10-22 15:29:45 -0500

    Lots of stuff

---
+++ app/src/main/java/com/example/wellinformed2/wellInspection.java
@@ -0,0 +1,14 @@
+package com.example.wellinformed2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class wellInspection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_well_inspection);
+    }
+}
