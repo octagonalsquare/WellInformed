6f80c7c45e2c78b61b8b887867887c9698c2978a
jesusquintero <quinteroj20@gmail.com>
2019-10-22 23:56:53 -0500

    made initial activity the login activity, just click sign in to proceed to the next page.

--- app/src/main/java/com/example/wellinformed2/LoginActivity.java
+++ app/src/main/java/com/example/wellinformed2/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.wellinformed2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnSignIn=findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view==btnSignIn){
+            startActivity(new Intent(this,MainActivity.class));
+        }
     }
 }
