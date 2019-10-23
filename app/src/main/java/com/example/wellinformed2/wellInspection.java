ea23ade8b48b2c31c1343f29847dbd6ab7267ad8
octagonalsquare <octagonalsquare.mc@gmail.com>
2019-10-23 15:11:37 -0500

    Well Inspection Class

--- app/src/main/java/com/example/wellinformed2/wellInspection.java
+++ app/src/main/java/com/example/wellinformed2/wellInspection.java
@@ -40,4 +40,67 @@ public class wellInspection extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+    
+    class WellInspection
+    {
+        public  String WellID;
+        public  String WellName;
+        public  String WellAddress;
+        public  String CasingType;
+        public  String FlowTestGPM;
+        public  String CasingHeight;
+        public  String SleeveHeight;
+        public  String PadDimensions;
+        public  String ScreenDepth;
+        public  String SleeveType;
+        public  String AnnularCementDepth;
+        public  String WaterLevel;
+        public  String TotalDepth;
+        public  String WellPermitNumber;
+        public  String AnnularCementVerified;
+        public  String FlowTestMethodTime;
+        public  String SanitaryWellSeal;
+        public  String SepticDistance;
+        public  String PropertyLineDistance;
+        public  String NearestWellDistance;
+        public  String AerobicSprayAreaDistance;
+        public  String SepticLateralLinesDistance;
+        public  String OtherContaminationSourcesDistance;
+        
+        WellInspection(String wellID, String wellName, String wellAddress, String casingType, 
+                       String flowTestGPM, String casingHeight, String sleeveHeight,
+                       String padDimensions, String screenDepth, String sleeveType, 
+                       String annularCementDepth, String waterLevel, String totalDepth, 
+                       String wellPermitNumber, String annularCementVerified, 
+                       String flowTestMethodTime, String sanitaryWellSeal, String septicDistance, 
+                       String propertyLineDistance, String nearestWellDistance, 
+                       String aerobicSprayAreaDistance, String septicLateralLinesDistance, 
+                       String otherContaminationSourcesDistance)
+        {
+            WellID = wellID;
+            WellName = wellName;
+            WellAddress = wellAddress;
+            CasingType = casingType;
+            FlowTestGPM = flowTestGPM;
+            CasingHeight = casingHeight;
+            SleeveHeight = sleeveHeight;
+            PadDimensions = padDimensions;
+            ScreenDepth = screenDepth;
+            SleeveType = sleeveType;
+            AnnularCementDepth = annularCementDepth;
+            WaterLevel = waterLevel;
+            TotalDepth = totalDepth;
+            WellPermitNumber = wellPermitNumber;
+            AnnularCementVerified = annularCementVerified;
+            FlowTestMethodTime = flowTestMethodTime;
+            SanitaryWellSeal = sanitaryWellSeal;
+            SepticDistance = septicDistance;
+            PropertyLineDistance = propertyLineDistance;
+            NearestWellDistance = nearestWellDistance;
+            AerobicSprayAreaDistance = aerobicSprayAreaDistance;
+            SepticLateralLinesDistance = septicLateralLinesDistance;
+            OtherContaminationSourcesDistance = otherContaminationSourcesDistance;
+        }
+        
+    }
 }
