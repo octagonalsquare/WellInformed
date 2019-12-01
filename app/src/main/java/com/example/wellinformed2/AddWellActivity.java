f1c60289fc3da964039b6faedeec5acfa27b7972
octagonalsquare <octagonalsquare.mc@gmail.com>
2019-12-01 12:09:24 -0600

    Added Comments to files

--- app/src/main/java/com/example/wellinformed2/AddWellActivity.java
+++ app/src/main/java/com/example/wellinformed2/AddWellActivity.java
@@ -214,7 +214,6 @@ public class AddWellActivity extends AppCompatActivity implements View.OnClickLi
         String key = drillerName;
         Driller newDriller = new Driller(drillerName, drillerCompany, drillerLicenseNumber, drillerLicenseExpirationDate);
         Map<String, Object> wellValues = newDriller.ToMap();
-        wellValues.remove(drillerName);
         Map<String, Object> childUpdates = new HashMap<>();
 
         mDatabaseRef.child("Driller").addListenerForSingleValueEvent(new ValueEventListener() {
